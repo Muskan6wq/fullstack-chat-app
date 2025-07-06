b6ff8e468517519d6c18a31c9b5e6e679edb167e
Muskan Bhushan <muskanbhushan2@gmail.com>
2025-07-06 17:03:06 +0530

    🚑 Fix: Case-sensitive import for ChatContainer

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,8 @@
 import { useChatStore } from "../store/useChatStore";
 
+import ChatContainer from "../components/ChatContainer";
 import NoChatSelected from "../components/NoChatSelected";
 import Sidebar from "../components/Sidebar";
-import ChatContainer from "../components/chatContainer";
 
 const HomePage = () => {
     const { selectedUser } = useChatStore();
